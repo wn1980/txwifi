8330ac612eedb4fcd7cb34c5277a9f5e7e9e96fe
Natalie Bloomfield <31744764+spidercatnat@users.noreply.github.com>
2020-08-27 14:30:05 -0700

    Add wpa_cli select_network step to ConnectNetwork method
    Fixes issue #23 (https://github.com/txn2/txwifi/issues/23)

--- iotwifi/wpacfg.go
+++ iotwifi/wpacfg.go
@@ -179,6 +179,15 @@ func (wpa *WpaCfg) ConnectNetwork(creds WpaCredentials) (WpaConnection, error) {
 	}
 	enableStatus := strings.TrimSpace(string(enableOut))
 	wpa.Log.Info("WPA enable got: %s", enableStatus)
+	
+	// 5. Select the new network
+	selectOut, err := exec.Command("wpa_cli", "-i", "wlan0", "select_network", net).Output()
+	if err != nil {
+		wpa.Log.Fatal(err.Error())
+		return connection, err
+	}
+	selectStatus := strings.TrimSpace(string(selectOut))
+	wpa.Log.Info("WPA select got: %s", selectStatus)
 
 	// regex for state
 	rState := regexp.MustCompile("(?m)wpa_state=(.*)\n")
